216f78d9124d6da38f2682198bf7411a245840ae
Mikhail Kamorin <m.v.kamorin@gmail.com>
2021-07-03 22:36:22 +0300

    Fix user entity and add user creation method

--- src/Controller/WorldController.php
+++ src/Controller/WorldController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Manager\UserManager;
-use JsonException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -16,11 +15,10 @@ class WorldController extends AbstractController
         $this->userManager = $userManager;
     }
 
-    /**
-     * @throws JsonException
-     */
     public function hello(): Response
     {
-        return $this->render('user-vue.twig', ['users' => json_encode($this->userManager->getUsersListVue(), JSON_THROW_ON_ERROR)]);
+        $user = $this->userManager->create('My user');
+
+        return $this->json($user->toArray());
     }
 }
