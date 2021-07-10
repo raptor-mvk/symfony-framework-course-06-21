54c647f4f5de29636859862c8b30ba49e0ba9f05
Mikhail Kamorin <m.v.kamorin@gmail.com>
2021-07-10 22:28:50 +0300

    Add stof/doctrine-extensions-bundle

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,11 +6,11 @@ use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\Table(name="`user`")
  * @ORM\Entity
- * @ORM\HasLifecycleCallbacks()
  */
 class User implements HasMetaTimestampsInterface
 {
@@ -28,11 +28,13 @@ class User implements HasMetaTimestampsInterface
 
     /**
      * @ORM\Column(name="created_at", type="datetime", nullable=false)
+     * @Gedmo\Timestampable(on="create")
      */
     private DateTime $createdAt;
 
     /**
      * @ORM\Column(name="updated_at", type="datetime", nullable=false)
+     * @Gedmo\Timestampable(on="update")
      */
     private DateTime $updatedAt;
 
@@ -99,9 +101,6 @@ class User implements HasMetaTimestampsInterface
         return $this->createdAt;
     }
 
-    /**
-     * @ORM\PrePersist()
-     */
     public function setCreatedAt(): void {
         $this->createdAt = new DateTime();
     }
@@ -110,10 +109,6 @@ class User implements HasMetaTimestampsInterface
         return $this->updatedAt;
     }
 
-    /**
-     * @ORM\PrePersist()
-     * @ORM\PreUpdate()
-     */
     public function setUpdatedAt(): void {
         $this->updatedAt = new DateTime();
     }
