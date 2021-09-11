9ec7015773bfc758f67a2d52bb5ef4cabd6ac1c4
Mikhail Kamorin <m.v.kamorin@gmail.com>
2021-09-11 22:27:22 +0300

    Add option

--- src/Command/AddFollowersCommand.php
+++ src/Command/AddFollowersCommand.php
@@ -7,16 +7,13 @@ use App\Manager\UserManager;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 final class AddFollowersCommand extends Command
 {
-    /** @var int */
-    public const OK = 0;
-    /** @var int */
-    public const GENERAL_ERROR = 1;
-
     private const DEFAULT_FOLLOWERS = 100;
+    private const DEFAULT_LOGIN_PREFIX = 'Reader #';
 
     private UserManager $userManager;
 
@@ -34,7 +31,8 @@ final class AddFollowersCommand extends Command
         $this->setName('followers:add')
             ->setDescription('Adds followers to author')
             ->addArgument('authorId', InputArgument::REQUIRED, 'ID of author')
-            ->addArgument('count', InputArgument::OPTIONAL, 'How many followers should be added');
+            ->addArgument('count', InputArgument::OPTIONAL, 'How many followers should be added')
+            ->addOption('login', 'l', InputOption::VALUE_REQUIRED, 'Follower login prefix');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -43,15 +41,16 @@ final class AddFollowersCommand extends Command
         $user = $this->userManager->findUserById($authorId);
         if ($user === null) {
             $output->write("<error>User with ID $authorId doesn't exist</error>\n");
-            return self::GENERAL_ERROR;
+            return self::FAILURE;
         }
         $count = (int)($input->getArgument('count') ?? self::DEFAULT_FOLLOWERS);
         if ($count < 0) {
             $output->write("<error>Count should be positive integer</error>\n");
-            return self::GENERAL_ERROR;
+            return self::FAILURE;
         }
-        $result = $this->subscriptionService->addFollowers($user, "Reader #{$authorId}", $count);
+        $login = $input->getOption('login') ?? self::DEFAULT_LOGIN_PREFIX;
+        $result = $this->subscriptionService->addFollowers($user, $login.$authorId, $count);
         $output->write("<info>$result followers were created</info>\n");
-        return self::OK;
+        return self::SUCCESS;
     }
 }
