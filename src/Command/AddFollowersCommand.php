5ca21a8a9149fd89266acabe161c8135fa7c5330
Mikhail Kamorin <m.v.kamorin@gmail.com>
2021-09-11 22:36:35 +0300

    Add lock to command

--- src/Command/AddFollowersCommand.php
+++ src/Command/AddFollowersCommand.php
@@ -5,13 +5,16 @@ namespace App\Command;
 use App\Service\SubscriptionService;
 use App\Manager\UserManager;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Command\LockableTrait;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-final class AddFollowersCommand extends Command
+class AddFollowersCommand extends Command
 {
+    use LockableTrait;
+
     private const DEFAULT_FOLLOWERS = 100;
     private const DEFAULT_LOGIN_PREFIX = 'Reader #';
 
@@ -38,6 +41,11 @@ final class AddFollowersCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        if (!$this->lock()) {
+            $output->writeln('<info>Command is already running.</info>');
+            return self::SUCCESS;
+        }
+        sleep(100);
         $authorId = (int)$input->getArgument('authorId');
         $user = $this->userManager->findUserById($authorId);
         if ($user === null) {
